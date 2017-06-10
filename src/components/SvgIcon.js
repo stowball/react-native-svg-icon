e83ac2ae5ab4bdfe400252555572f3ee818db7a6
Ronaldo Lima <ronal2do@gmail.com>
2017-06-10 11:11:01 -0300

    add prop-types, react-propstypes is deprecated

--- src/components/SvgIcon.js
+++ src/components/SvgIcon.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';  
 import Svg from 'react-native-svg';
 
 const defaultViewBox = '0 0 100 100';
